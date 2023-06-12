97e45685ce90db701824c20734839810576da0fa
ahn7157 <ahn7157@gmail.com>
2023-06-12 22:43:59 +0900

    feat: update health.sh and main.py

--- health.sh
+++ health.sh
@@ -1,10 +1,38 @@
 #!/bin/bash
 
-COUNT=0
-while :
-do
+function printManual(){
+  echo "$(tput bold)This is Health Booking Script$(tput sgr0)"
+  echo ""
+  echo "$(tput bold)USAGE$(tput sgr0)"
+  echo "  ./health.sh [options]"
+  echo ""
+  echo "$(tput bold)OPTIONS$(tput sgr0)"
+  echo "  0 : use everyday"
+  echo "  25: use on the 25th"
+}
+
+function book0(){
+  COUNT=0
+  while :
+  do
     pipenv run python main.py
     let COUNT=COUNT+1
     echo attempt: $COUNT
     sleep 1200
-done
+  done
+}
+
+function book25(){
+  pipenv run python main.py 25
+}
+
+ARG_1=${1}
+
+if [ "${ARG_1}" == "0" ]; then
+  book0
+elif [ "${ARG_1}" == "25" ]; then
+  book25
+else
+  printManual
+fi
+
--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 import os
+import sys
+
 import requests
 import asyncio
 import telegram
 import json
 import datetime
 from dotenv import load_dotenv
+import calendar
 
 
 def init():
     load_dotenv()
 
-    global url
+    global useMonth
     global useStDate
+    global useEdDate
     global helMngerCd
     global useStTime
     global useEdTime
     global userDiv
     global reqEmpNo
     global holidays
-
-    url = "https://talk.tmaxsoft.com/front/health/insertHealth.do?helMngerCd=HEL20220609001"
 
     helMngerCdMap = {
         "손애화": "HEL20220609001",
@@ -30,7 +32,9 @@ def init():
 
     helMnger = os.getenv("helMnger")
     helMngerCd = helMngerCdMap[helMnger]
+    useMonth = os.getenv("useMonth")
     useStDate = os.getenv("useStDate")
+    useEdDate = calendar.monthrange(int(useMonth[0:4]), int(useMonth[4:6]))[1]
     useStTime = os.getenv("useStTime")
     useEdTime = useStTime[0:2] + "50"
     userDiv = "0001"
@@ -53,10 +57,10 @@ def create_session():
 def get_holiday():
     # 한국천문연구원_특일 정보 - 공휴일 정보 조회
     key = os.getenv("key")
-    url = "http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getRestDeInfo?_type=json&numOfRows=50" \
-          "&solYear=" + str(useStDate)[0:4] + "&solMonth=" + str(useStDate)[4:6] \
-          + '&ServiceKey=' + key
-    res = requests.get(url)
+    apis_url = "http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getRestDeInfo?_type=json&numOfRows=50" \
+               "&solYear=" + str(useStDate)[0:4] + "&solMonth=" + str(useStDate)[4:6] \
+               + '&ServiceKey=' + key
+    res = requests.get(apis_url)
     json_ob = json.loads(res.text)
     holiday_list = []
     temp = json_ob['response']['body']['items']
@@ -77,47 +81,77 @@ async def send_message(useDateStr):
     await bot.sendMessage(chat_id=os.getenv("chat_id"), text=useDateStr + " 예약 성공")
 
 
-def book():
-    for i in range(30):
-        useDate = str(int(useStDate) + i)
+def is_valid_date(date):
+    # 지난 날짜 체크
+    if int(date) < int(datetime.date.today().strftime("%Y%m%d")):
+        print(date + ' is already past')
+        return False
 
-        # 지난 날짜 체크
-        if int(useDate) < int(datetime.date.today().strftime("%Y%m%d")):
-            print(useDate + ' is already past')
-            continue
+    # 주말 체크
+    if datetime.date(int(date[0:4]), int(date[4:6]), int(date[6:8])).weekday() > 4:
+        print(date + ' is weekend')
+        return False
 
-        # 주말 체크
-        if datetime.date(int(useDate[0:4]), int(useDate[4:6]), int(useDate[6:8])).weekday() > 4:
-            print(useDate + ' is weekend')
-            continue
+    # 공휴일 체크
+    if date in holidays:
+        print(date + ' is holiday')
+        return False
 
-        # 공휴일 체크
-        if useDate in holidays:
-            print(useDate + ' is holiday')
-            continue
+    return True
 
-        url = "https://talk.tmaxsoft.com/front/health/insertHealth.do?" \
-              + "helMngerCd=" + helMngerCd \
-              + "&useDate=" + useDate \
-              + "&useStTime=" + useStTime \
-              + "&useEdTime=" + useEdTime \
-              + "&userDiv=" + userDiv \
-              + "&reqEmpNo=" + reqEmpNo
 
-        try:
-            res = create_session().get(url)
-            if res.json()["resultCount"] == '1':
-                asyncio.run(send_message(useDate))
-                print(useDate + " 예약 성공")
-            else:
-                print(useDate + " " + str(res.json()['errorMsg']))
-        except:
-            print(useDate + ' error')
+def request_book(useDate):
+    if not is_valid_date(useDate):
+        return
+
+    url = "https://talk.tmaxsoft.com/front/health/insertHealth.do?" \
+          + "helMngerCd=" + helMngerCd \
+          + "&useDate=" + useDate \
+          + "&useStTime=" + useStTime \
+          + "&useEdTime=" + useEdTime \
+          + "&userDiv=" + userDiv \
+          + "&reqEmpNo=" + reqEmpNo
+
+    try:
+        res = create_session().get(url)
+        if res.json()["resultCount"] == '1':
+            asyncio.run(send_message(useDate))
+            print(useDate + " 예약 성공")
+        else:
+            print(useDate + " " + str(res.json()['errorMsg']))
+    except:
+        print(useDate + ' error')
+
+
+def book0():
+    for i in range(int(useStDate), int(useEdDate) + 1):
+        use_date = str(int(useMonth) * 100 + i)
+        request_book(use_date)
+
+
+def book25():
+    # 25일에 사용
+    week_day = os.getenv('week_day_bit')
+    if not week_day:
+        print("week_day_bit is null")
+        return
+
+    dates = []
+    for i in range(int(useStDate), int(useEdDate) + 1):
+        use_date = str(int(useMonth) * 100 + i)
+        if int(week_day[datetime.date(int(use_date[0:4]), int(use_date[4:6]), int(use_date[6:8])).weekday()]):
+            dates.append(use_date)
+
+    for date in dates:
+        request_book(date)
 
 
 def main():
     init()
-    book()
+    if len(sys.argv) == 1:
+        book0()
+    elif sys.argv[1] == '25':
+        book25()
 
 
 if __name__ == "__main__":
